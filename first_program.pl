8242d553eed773c8d040dcc685a880b3e6b14252
Siiri <siiri.makinen@student.lamk.fi>
2019-03-13 17:47:34 +0100

    my first program

---
+++ first_program.pl
@@ -0,0 +1,17 @@
+
+
+% man(Person)
+% it is true if Person is a man 
+
+man(justus).
+man(lars).
+man(teun).
+man(sebastian).
+
+% woman (Person)
+% it is true if Person is a woman
+woman(wendele).
+woman(frederique).
+woman(lisanne)
+
+
